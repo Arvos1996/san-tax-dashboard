f2adaa3395fd33fbccd5141e6bc7c5dde9f0aacd
Jannik <arvos1337@gmail.com>
2021-04-02 23:11:18 +0200

    added UnitTest ; added postgresql Driver to application.properties ; added IntegrationTest

--- src/test/java/de/santax/dashboard/DemoApplicationTests.java
+++ src/test/java/de/santax/dashboard/DemoApplicationTests.java
@@ -4,12 +4,32 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class DemoApplicationTests {
 
+	CalculatorTest underTest = new CalculatorTest();
+
 	@Test
-	void contextLoads() {
-		// Assertions.fail("ooops test failed");
+	void itShouldAddNumbers() {
+
+		//given
+		int numberOne = 20;
+		int numberTwo = 30;
+
+		//when
+		int result = underTest.add(numberOne, numberTwo);
+
+		//then
+		assertThat(result).isEqualTo(50);
+
+	}
+
+	class CalculatorTest {
+		int add(int a, int b) {
+			return a + b;
+		}
 	}
 
 }
